9b06898573832313430a3bfba2df9b8fd03abcac
zhi-bao <tomk12171217@gmail.com>
2024-03-24 23:46:43 +0800

    Homework 1 for Classroom .

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    bool firstTime;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!firstTime){
+            firstTime = true;
+            return 1217;
+        }else{
+            return 123;
+        }
     }
 }
 
@@ -18,12 +24,18 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(IClassroomV2(msg.sender).isEnrolled())
+            return 123;
+        else
+            return 1217;
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
+    uint64 constant magic_number = 7000;
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        return gasleft() < magic_number? 123: 1217;
     }
 }
